5c6061da5246458d13f5c4f75bf6fd7115b940ca
Christina Gonzalez <cmgamway@gmail.com>
2019-08-07 05:52:41 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,9 +6,9 @@ end
   when 'should default to meat for the protein' do
 end
 
-  when 'should allow you to set a protein' do  
+  when 'should allow you to set a protein' do
 end
 
-  when 'should puts "What a nutritious meal!" and your order to the console' do  
+  when 'should puts "What a nutritious meal!" and your order to the console' do
 end
 end
